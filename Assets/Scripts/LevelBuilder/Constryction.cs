5a1cccc01165326575c2d8ac522dc99eb5952235
Develoder <55357879+Develoder@users.noreply.github.com>
2022-12-10 21:25:14 +0600

    Add construction

---
+++ Assets/Scripts/LevelBuilder/Constryction.cs
@@ -0,0 +1,6 @@
+﻿public enum Construction
+{
+    Ground, 
+    Buildings, 
+    Environments
+}
--- Assets/Scripts/LevelBuilder/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder/LevelBuilder.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 
 public class LevelBuilder : EditorWindow
 {
-    private const string _path = "Assets/Editor Resources/Buildings";
+    private const string _path = "Assets/Editor Resources/";
     private const int _rayLayerMask = 1 << 6; // Ground
     
     private Vector2 _scrollPosition;
@@ -15,8 +17,14 @@ public class LevelBuilder : EditorWindow
     private bool _building;
 
     private GameObject _createdObject;
-    private GameObject[] _parents = new GameObject[3]; // 0 - Gound, 1 - Buildings, 2 - Environments
+    private GameObject[] _parents = new GameObject[3]; // 0 - Ground, 1 - Buildings, 2 - Environments
     private GameObject _previewObject;
+    // private Vector3 _previewScale = Vector3.zero;
+    // private Quaternion _previewRotation = Quaternion.identity;
+
+    private Construction _selectedConstruction = Construction.Ground;
+    private string[] _tabNames = new string[] {"Ground", "Buildings", "Environments"};
+    private string[] _nameFolderTab = new string[] { "Ground", "Buildings", "Environments" };
 
     [MenuItem("Level/Builder")]
     private static void ShowWindow()
@@ -36,14 +44,22 @@ public class LevelBuilder : EditorWindow
         DrawParents();
         
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-        if (_createdObject != null)
+        
+        
+        int selectedTool = GUILayout.Toolbar((int)_selectedConstruction, _tabNames);
+        Construction currentConstruction = (Construction)Enum.ToObject(typeof(Construction), selectedTool);
+
+        if (currentConstruction != _selectedConstruction)
         {
-            EditorGUILayout.LabelField("Created Object Settings");
-            Transform createdTransform = _createdObject.transform;
-            createdTransform.position = EditorGUILayout.Vector3Field("Position", createdTransform.position);
-            createdTransform.rotation = Quaternion.Euler(EditorGUILayout.Vector3Field("Position", createdTransform.rotation.eulerAngles));
-            createdTransform.localScale = EditorGUILayout.Vector3Field("Position", createdTransform.localScale);
+            _selectedConstruction = currentConstruction;
+            RefreshCatalog();
         }
+
+        if (_selectedConstruction != Construction.Ground)
+            if (_createdObject != null)
+                //DrawObjectSettings();
+                EditionParametersPreview();
+
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
         _building = GUILayout.Toggle(_building, "Start building", "Button", GUILayout.Height(60));
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
@@ -56,7 +72,7 @@ public class LevelBuilder : EditorWindow
         CheckChangeItemCatalog();
     }
 
-   private void OnSceneGUI(SceneView sceneView)
+    private void OnSceneGUI(SceneView sceneView)
     {
         if (!_building)
         {
@@ -66,15 +82,20 @@ public class LevelBuilder : EditorWindow
         }
         if (!Raycast(out Vector3 contactPoint)) 
             return;
-        
-        //DrawPointer(contactPoint, Color.red);
-        DrawPreview(contactPoint);
 
-        if (CheckInput())
+        if (_selectedConstruction == Construction.Ground)
         {
-            CreateObject(contactPoint);
+            // Блок превью для тайла
         }
+        else
+        {
+            //DrawPointer(contactPoint, Color.red);
+            DrawPreview(contactPoint);
 
+            if (CheckInput())
+                CreateObject(contactPoint);
+        }
+        
         sceneView.Repaint();
     }
    
@@ -83,9 +104,12 @@ public class LevelBuilder : EditorWindow
        EditorGUILayout.LabelField("Parents");
        
        EditorGUILayout.BeginVertical(GUI.skin.box);
-       _parents[0] = (GameObject)EditorGUILayout.ObjectField("Ground", _parents[0], typeof(GameObject), true);
-       _parents[1] = (GameObject)EditorGUILayout.ObjectField("Buildings", _parents[1], typeof(GameObject), true);
-       _parents[2] = (GameObject)EditorGUILayout.ObjectField("Environments", _parents[2], typeof(GameObject), true);
+       int enemyTypesCount = Enum.GetNames(typeof(Construction)).Length;
+       for (int i = 0; i < enemyTypesCount; i++)
+       {
+           Construction type = (Construction)Enum.ToObject(typeof(Construction), i);
+           _parents[i] = (GameObject)EditorGUILayout.ObjectField(type.ToString(), _parents[i], typeof(GameObject), true);
+       }
        EditorGUILayout.EndVertical();
    }
     
@@ -116,8 +140,10 @@ public class LevelBuilder : EditorWindow
     {
         if (_previewObject == null)
             CreatePreviewObject();
-
-        _previewObject.transform.position = contactPoint;
+        
+        _previewObject.transform.position = contactPoint;        
+        // _previewObject.transform.rotation = _previewRotation;
+        // _previewObject.transform.localScale = _previewScale;
     }
 
     private void CheckChangeItemCatalog()
@@ -156,16 +182,66 @@ public class LevelBuilder : EditorWindow
         
         GameObject prefab = _catalog[_selectedElement];
 
-        _createdObject = Instantiate(prefab, _parents[1].transform, true);
+        _createdObject = Instantiate(prefab, _parents[(int)_selectedConstruction].transform, true);
         _createdObject.transform.position = position;
 
         Undo.RegisterCreatedObjectUndo(_createdObject, "Create Building");
     }
+    
+    private void DrawObjectSettings()
+    {
+        EditorGUILayout.LabelField("Created Object Settings");
+        Transform createdTransform = _createdObject.transform;
+        //createdTransform.position = EditorGUILayout.Vector3Field("Position", createdTransform.position);
+
+        createdTransform.rotation = Quaternion.Euler(EditorGUILayout.Vector3Field("Rotation", createdTransform.rotation.eulerAngles));
+        createdTransform.localScale = EditorGUILayout.Vector3Field("Scale", createdTransform.localScale);
+    }
+
+    private void EditionParametersPreview()
+    {
+        if (Event.current.type != EventType.KeyDown)
+            return;
+        Debug.Log(Event.current.keyCode);
+        switch (Event.current.keyCode)
+        {
+            case KeyCode.V:
+                _previewObject.transform.localScale +=  Vector3.one * 0.1f;
+                break;
+            case KeyCode.C:
+                _previewObject.transform.localScale -=  Vector3.one * 0.1f;
+                break;
+            //Вращать влево
+            case KeyCode.F:
+                _previewObject.transform.RotateAround(_previewObject.transform.position, Vector3.up, 20);
+                break;
+            //Вращать вправо
+            case KeyCode.G:
+                _previewObject.transform.RotateAround(_previewObject.transform.position, Vector3.down, 20);
+                break;
+        }
+    }
 
     private void DrawCatalog(List<GUIContent> catalogIcons)
     {
-        GUILayout.Label("Buildings");
-        _selectedElement = GUILayout.SelectionGrid(_selectedElement, catalogIcons.ToArray(), 4, GUILayout.Width(400), GUILayout.Height(1000));
+        GUILayout.Label(_selectedConstruction.ToString());
+
+        float targetSizeElement = 100f; // Примерный размер эллемента
+        float minSizeElement = 50f; // Минимальный размер элемента
+        float paddingLeft = 30f; // Отступ с лева, чтоб слайдер не загораживал
+        float withWindow = position.width; // Ширина окна
+        int column = Mathf.RoundToInt((withWindow - withWindow % targetSizeElement) / targetSizeElement); // Считает колличество столбцов
+        column = Mathf.Clamp(column, 1, int.MaxValue); // Не дает колличеству столбцов равняться нулю
+        float sizeElement = targetSizeElement + // К начальному размеру добавляет остаток
+            (withWindow - column * targetSizeElement) / column // Определяет остаток не занятого пространства начального значения
+            - paddingLeft / column; // Делит отступ на всех элементов
+        sizeElement = Mathf.Clamp(sizeElement, minSizeElement, float.MaxValue); // Смотрит чтобы меньше минимального разменра не была
+        int row = (catalogIcons.Count - catalogIcons.Count % column) / column; // Определаяте колличество строк
+        
+        float with = sizeElement * column;
+        float height = row * sizeElement;
+        
+        _selectedElement = GUILayout.SelectionGrid(_selectedElement, catalogIcons.ToArray(), column, GUILayout.Width(with), GUILayout.Height(height));
     }
 
     private List<GUIContent> GetCatalogIcons()
@@ -185,9 +261,15 @@ public class LevelBuilder : EditorWindow
     {
         _catalog.Clear();
 
-        System.IO.Directory.CreateDirectory(_path);
-        string[] prefabFiles = System.IO.Directory.GetFiles(_path, "*.prefab");
+        System.IO.Directory.CreateDirectory(FullPath());
+        string[] prefabFiles = System.IO.Directory.GetFiles(FullPath(), "*.prefab");
         foreach (var prefabFile in prefabFiles)
             _catalog.Add(AssetDatabase.LoadAssetAtPath(prefabFile, typeof(GameObject)) as GameObject);
+    }
+
+    private string FullPath()
+    {
+        //Debug.Log($"{_selectedConstruction} {(int)_selectedConstruction}");
+        return _path + _nameFolderTab[(int)_selectedConstruction];
     }
 }
